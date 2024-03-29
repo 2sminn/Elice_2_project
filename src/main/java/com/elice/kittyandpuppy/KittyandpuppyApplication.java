938f952074521ea6eaf1caa1cef442314b95437d
Lazy Cat <mylazysundaycat@gmail.com>
2024-03-29 20:02:29 +0900

    feat: BaseTimeEntity 추가

--- src/main/java/com/elice/kittyandpuppy/KittyandpuppyApplication.java
+++ src/main/java/com/elice/kittyandpuppy/KittyandpuppyApplication.java
@@ -2,8 +2,10 @@ package com.elice.kittyandpuppy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class KittyandpuppyApplication {
 
 	public static void main(String[] args) {
