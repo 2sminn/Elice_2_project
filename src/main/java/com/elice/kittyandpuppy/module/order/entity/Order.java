caf65e3e5120615ecc69e373fe549c6c2ce73c75
GiJung <101462387+GiJungPark@users.noreply.github.com>
2024-04-01 02:33:50 +0900

    feat(Order): Create Order class
    배송지 정보를 관리하고
    Member와 다대일 연관관계, OrderDetail과 일대다 연관관계를 가지는 Order class 생성

--- src/main/java/com/elice/kittyandpuppy/module/order/entity/Order.java
+++ src/main/java/com/elice/kittyandpuppy/module/order/entity/Order.java
@@ -1,4 +1,68 @@
 package com.elice.kittyandpuppy.module.order.entity;
 
+import com.elice.kittyandpuppy.global.Address;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+// TODO: Member 객체 연결하는 과정 필요
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+@Table(name = "order")
 public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+//    @ManyToOne
+//    @JoinColumn(name = "member_id")
+//    private Member member;
+
+    @OneToMany(mappedBy = "order_detail")
+    private List<OrderDetail> orderDetails;
+
+    @Column(name = "zip_code", nullable = false)
+    private String zipCode;
+
+    @Column(name = "street_address", nullable = false)
+    private String streetAddress;
+
+    @Column(name = "detail_address", nullable = false)
+    private String detailAddress;
+
+    @Column(name = "receiver_name", nullable = false)
+    private String receiverName;
+
+    @Column(name = "receiver_phone", nullable = false)
+    private String receiverPhone;
+
+    @Builder
+    public Order(/*Member member, */Address address, String receiverName, String receiverPhone) {
+//        this.member = member;
+        this.zipCode = address.getZipCode();
+        this.streetAddress = address.getStreet();
+        this.detailAddress = address.getDetail();
+        this.receiverName = receiverName;
+        this.receiverPhone = receiverPhone;
+    }
+
+    public void setOrderDetails(OrderDetail orderDetails) {
+        this.orderDetails.add(orderDetails);
+    }
 }
+
+
+
+
