03dd7828f09df2112913059ded0025bd798815bc
GiJung <101462387+GiJungPark@users.noreply.github.com>
2024-04-03 17:18:07 +0900

    feat(Order): 주문 상태 변경 로직, 총 금액 계산 로직 추가 및 생성 메서드로 변경
    주문 상태 변경 로직 작성
    주문 취소시 재고를 복구하는 로직 추가 작성
    주문 총 금액을 계산하는 로직 작성
    주문 객체 생성시 연관 관계 맵핑을 고려하여 생성 메서드로 변경

--- src/main/java/com/elice/kittyandpuppy/module/order/entity/Order.java
+++ src/main/java/com/elice/kittyandpuppy/module/order/entity/Order.java
@@ -1,21 +1,30 @@
 package com.elice.kittyandpuppy.module.order.entity;
 
-import com.elice.kittyandpuppy.global.Address;
+import com.elice.kittyandpuppy.module.member.entity.Member;
+import com.elice.kittyandpuppy.module.order.config.OrderStatus;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AccessLevel;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
-// TODO: Member 객체 연결하는 과정 필요
+
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Entity
@@ -26,40 +35,137 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-//    @ManyToOne
-//    @JoinColumn(name = "member_id")
-//    private Member member;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
 
-    @OneToMany(mappedBy = "order_detail")
-    private List<OrderDetail> orderDetails;
+    @OneToMany(mappedBy = "order_detail", cascade = CascadeType.ALL)
+    private List<OrderDetail> orderDetails = new ArrayList<>();
 
-    @Column(name = "zip_code", nullable = false)
-    private String zipCode;
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private Delivery delivery;
 
-    @Column(name = "street_address", nullable = false)
-    private String streetAddress;
+    @Column(name = "order_date", nullable = false)
+    private LocalDateTime orderDate;
 
-    @Column(name = "detail_address", nullable = false)
-    private String detailAddress;
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
 
-    @Column(name = "receiver_name", nullable = false)
-    private String receiverName;
-
-    @Column(name = "receiver_phone", nullable = false)
-    private String receiverPhone;
-
-    @Builder
-    public Order(/*Member member, */Address address, String receiverName, String receiverPhone) {
-//        this.member = member;
-        this.zipCode = address.getZipCode();
-        this.streetAddress = address.getStreet();
-        this.detailAddress = address.getDetail();
-        this.receiverName = receiverName;
-        this.receiverPhone = receiverPhone;
+    // 객체 연관 관계 맵핑 로직
+    // TODO: Member 객체와 연결햐여 험
+    public void setMember(Member member) {
+        this.member = member;
+//        member.getOrders().add(this);
     }
 
-    public void setOrderDetails(OrderDetail orderDetails) {
-        this.orderDetails.add(orderDetails);
+    public void addOrderDetail(OrderDetail orderDetail) {
+        this.orderDetails.add(orderDetail);
+        orderDetail.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
+
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    // 객체 생성 로직
+    /**
+     * Order 객체를 생성
+     *
+     * @param member
+     * @param delivery
+     * @param orderItems
+     * @return 저장된 Order 객체
+     */
+    public static Order createOrder(Member member, Delivery delivery, List<OrderDetail> orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+
+        for(OrderDetail orderItem : orderItems) {
+            order.addOrderDetail(orderItem);
+        }
+
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+
+        return order;
+    }
+
+    // 주문 상태 변경 로직
+    /**
+     * 주문 상태 취소로 변경
+     * @throws IllegalArgumentException 주문 상태 변경 실패<br>
+     *         - 상태가 배송중인 경우: "배송중인 상품은 취소가 불가능합니다." <br>
+     *         - 상태가 배송완료인 경우: "배송이 완료된 상품은 취소가 불가능합니다."
+     */
+    public void cancel() {
+        if(this.status == OrderStatus.DELIVERY) {
+            throw new IllegalStateException("배송중인 상품은 취소가 불가능합니다.");
+        }
+
+        if(this.status == OrderStatus.COMPLETE) {
+            throw new IllegalStateException("배숭이 완료된 상품은 취소가 불가능합니다.");
+        }
+
+        this.setStatus(OrderStatus.CANCEL);
+        // 재고 복구 로직
+        for(OrderDetail orderItem : this.orderDetails) {
+            orderItem.cancel();
+        }
+    }
+
+    /**
+     * 주문 상태 주문으로 변경
+     *
+     * @throws IllegalArgumentException 주문 상태 변경 실패<br>
+     *         - ORDER 이외의 상태인 경우: "이미 배송이 시작되었거나, 취소된 상품입니다."
+     */
+    public void delivery() {
+        if(this.status != OrderStatus.ORDER) {
+            throw new IllegalStateException("이미 배송이 시작되었거나, 취소된 상품입니다.");
+        }
+
+        this.setStatus(OrderStatus.DELIVERY);
+    }
+
+    /**
+     * 주문 상태 완료로 변경
+     *
+     * @throws IllegalArgumentException 주문 상태 변경 실패<br>
+     *         - COMPLETE 상태인 경우: "이미 배송이 완료된 상품입니다."
+     */
+    public void complete() {
+        if(this.status == OrderStatus.COMPLETE) {
+            throw new IllegalStateException("이미 배송이 완료된 상품입니다.");
+        }
+
+        this.setStatus(OrderStatus.COMPLETE);
+    }
+
+    // 비지니스 로직
+    /**
+     * 주문한 상품들의 총 금액을 계산한다.
+     *
+     * @return 주문한 상품의 총 금액
+     */
+    public int getTotalPrice() {
+        int totalPrice = 0;
+
+        for (OrderDetail orderItem : this.orderDetails) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+
+        return totalPrice;
     }
 }
 
