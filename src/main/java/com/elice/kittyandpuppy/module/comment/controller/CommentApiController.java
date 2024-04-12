ac9ed03b053c2f56377b8b0a77b0fbdcbdb6c3b0
Lazy Cat <mylazysundaycat@gmail.com>
2024-04-12 15:49:28 +0900

    feat: CommentViewController 추가

--- src/main/java/com/elice/kittyandpuppy/module/comment/controller/CommentApiController.java
+++ src/main/java/com/elice/kittyandpuppy/module/comment/controller/CommentApiController.java
@@ -10,13 +10,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/comment")
-public class CommentController {
+public class CommentApiController {
     private final CommentService commentService;
     @Operation(summary = "댓글 작성")
     @PostMapping
