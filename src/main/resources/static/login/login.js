055ebdb34f146bc3366e910b8f119ea1b724984a
jaewon926 <jjojjo0926@gmail.com>
2024-04-12 19:24:20 +0900

    ㄹãfeat(Member-FE-login) ㅇJWT 저장 로직 추가

--- src/main/resources/static/login/login.js
+++ src/main/resources/static/login/login.js
@@ -10,21 +10,23 @@ function login() {
         email : $("#email").val(),
         password : $("#password").val()
     };
+    let url;
     //Ajax로 전송
-    $.ajax({
+    url=$.ajax({
         url : '/api/member/login',
         data : JSON.stringify(data),
         type : 'POST',
         contentType: "application/json ; charset=UTF-8",
         dataType : 'json',
-        success : function(result) {
-                console.log(result);
-            $("#error").addClass("visually-hidden");
+        success : function(data) {
+            if(data){
+                localStorage.setItem("jwtToken",url.getResponseHeader("token"));
                 alert("로그인에 성공하였습니다!!");
-        },
-        error:function(request){
-            alert("로그인에 실패하였습니다!!");
-            $("#error").removeClass("visually-hidden");
+                window.location.href="/";
+            }else{
+                alert("로그인에 실패하였습니다!!");
+                $("#error").removeClass("visually-hidden");
+            }
         }
-    }); //End Ajax
+    });
 }
