076652bf301f26b19ba74d8bd97843eab581474a
Lazy Cat <mylazysundaycat@gmail.com>
2024-04-14 22:04:20 +0900

    Merge branch 'comment' into post
    # Conflicts:
    #	src/main/java/com/elice/kittyandpuppy/module/comment/controller/CommentApiController.java
    #	src/main/java/com/elice/kittyandpuppy/module/comment/service/CommentService.java
    #	src/main/resources/static/js/comment.js

--- src/main/resources/static/js/comment.js
+++ src/main/resources/static/js/comment.js
@@ -1,24 +1,126 @@
-//   요청한 데이터 : {"member_list":[
-//    {"id":"aa1","pw":"bb","addr":"cc","tel":"dd"},
-//    {"id":"aa2","pw":"bb","addr":"cc","tel":"dd"},
-//    {"id":"aa3","pw":"bb","addr":"cc","tel":"dd"}
-//  ]}
-$(document).ready(function(){
-    $("#listButton").click(getMemberList); //id="listButton"인 태그에 click하면 function getMemberList() 실행
+//페이지 로드 시에
+$(document).ready(function () {
+    //댓글 작성
+    $("#comment_submit").click(function () {
+        comment_create();
+    });
+    //댓글 조회
+    const postId = window.location.pathname.split('/').pop();
+    loadComments(postId);
+
 });
-function getMemberList(){
+
+//댓글 작성
+function comment_create() {
+    let data = {
+        content : $('#comment_content').val()
+    }
     $.ajax({
-        url:"list.jsp",                    //list.jsp에 AJAX요청
-        success:function(data){
-            let obj=JSON.parse(data);      //data를 받아와서 JSON형태로 변환
-            let array=["<ol>"];
-            obj["member_list"].forEach(
-                member =>  array.push("<li>"+member.id+"</li>")
-            //JSON에 있는 member.id의 value를 li태그에 넣어서 array에 넣어줌
-            );
-            array.push("</ol>");
-            $("#result").html(array.join(""));
-            //array의 요소들을 다 합쳐서 하나로 만든후 id="result"인 태그에 html로 출력
+        url: '/api/comment',
+        type: 'POST',
+        dataType: 'text',
+        contentType: "application/json",
+        data: JSON.stringify(data),
+        success: function (data) {
+            alert("댓글이 작성되었습니다");
+            location.href = '/comment';
+        }
+    });
+
+
+}
+//댓글 조회 테스트
+function loadComments(postId) {
+    $.ajax({
+        url: '/api/comment/' + postId,
+        type: 'GET',
+        dataType: 'json',
+        success: function (post) {
+            displayComments(post);
+        },
+        error: function(xhr, status, error) {
+            console.error('Error fetching comments:', error);
         }
     });
 }
+function displayComments(post) {
+    let comments = post.comments;
+
+    let commentList = $('#comment_list');
+    commentList.empty();
+
+    if (comments.length === 0) {
+        commentList.append('<p>No comments yet.</p>');
+    } else {
+        comments.forEach(function(comment) {
+            let commentItem = "";
+            commentItem.append(`<div class="comment-content-container" id="comment_detail">
+                <div class="row-box comment-header">
+                    <div class="commnet-name-box">
+                        <img src ="/img/miao.jpg" class="comment-profile">
+                        <span class="commnet-name">냔냔펑치</span>
+                    </div>
+                    <div class="comment-day">comment.modifiedAt</div>
+                </div>
+                <div class="row-box comment-article">
+                    <div> ${comment.content} </div>
+                </div>
+                <div class="row-box comment-footer">
+                    <input type="button" value="댓글" class="reply-btn">
+                    <input type="button" value="좋아요" class="upvote-btn">
+                    <input type="button" value="싫어요" class="downvote-btn"></div>
+            </div>`)
+            commentList.append(commentItem);
+        });
+    }
+}
+
+
+//
+// //댓글 조회
+// function loadComments(postId) {
+//     $.ajax({
+//         url: '/api/comment/' + postId,
+//         type: 'GET',
+//         dataType: 'json',
+//         success: function (data) {
+//             displayComments(data);
+//         },
+//         error: function(xhr, status, error) {
+//             console.error('Error fetching comments:', error);
+//         }
+//     });
+// }
+//
+//
+// function displayComments(post) {
+//     let comments = post.getComment; // post 로부터 comment 리스트를 받아와야 하는데 ~...
+//     let commentList = $('#comment_list');
+//     commentList.empty();
+//
+//     if (comments.length === 0) {
+//         commentList.append('<p>No comments yet.</p>');
+//     } else {
+//         comments.forEach(function(comment) {
+//             let commentItem = "";
+//             commentItem += `<div class="comment-content-container" id="comment_detail">
+//                 <div class="row-box comment-header">
+//                     <div class="commnet-name-box">
+//                         <img src ="/img/miao.jpg" class="comment-profile">
+//                         <span class="commnet-name">냔냔펑치</span>
+//                     </div>
+//                     <div class="comment-day">2024.03.09</div>
+//                 </div>
+//                 <div class="row-box comment-article" >
+//                     <div>` +comment.getContent+`</div> // comment 객체로부터 content도 받아와야 하고..
+//                 </div>
+//                 <div class="row-box comment-footer">
+//                     <input type="button" value="댓글" class="reply-btn">
+//                     <input type="button" value="좋아요" class="upvote-btn">
+//                     <input type="button" value="싫어요" class="downvote-btn">
+//                 </div>
+//             </div>`
+//             commentList.append(commentItem);
+//         });
+//     }
+// }
