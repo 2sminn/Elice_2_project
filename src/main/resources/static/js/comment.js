5706fd69b7dbf3cb7d2de74aa541251d6f760979
Lazy Cat <mylazysundaycat@gmail.com>
2024-04-15 00:04:15 +0900

    feat: CommentResposne 생성, Comment.js 댓글 작성/조회 기능 추가

--- src/main/resources/static/js/comment.js
+++ src/main/resources/static/js/comment.js
@@ -2,19 +2,26 @@
 $(document).ready(function () {
     //댓글 작성
     $("#comment_submit").click(function () {
-        comment_create();
+        comment_create(postId);
     });
+
     //댓글 조회
     const postId = window.location.pathname.split('/').pop();
-    loadComments(postId);
+    $.ajax({
+        url: `/api/comment/${postId}`,
+        type: 'GET',
+        dataType: 'json',
+        success: function (comments) {
+            displayComments(comments);
+        }
+    });
 
 });
 
 //댓글 작성
-function comment_create() {
-    let data = {
-        content : $('#comment_content').val()
-    }
+function comment_create(postId) {
+    let data = {content : $('#comment_content').val(),
+        postId : postId}
     $.ajax({
         url: '/api/comment',
         type: 'POST',
@@ -23,105 +30,35 @@ function comment_create() {
         data: JSON.stringify(data),
         success: function (data) {
             alert("댓글이 작성되었습니다");
-            location.href = '/comment';
-        }
-    });
-
-
-}
-//댓글 조회 테스트
-function loadComments(postId) {
-    $.ajax({
-        url: '/api/comment/' + postId,
-        type: 'GET',
-        dataType: 'json',
-        success: function (post) {
-            displayComments(post);
-        },
-        error: function(xhr, status, error) {
-            console.error('Error fetching comments:', error);
+            location.href = '/community/' + postId;
         }
     });
 }
-function displayComments(post) {
-    let comments = post.comments;
 
+
+//댓글 조회
+function displayComments(comments) {
     let commentList = $('#comment_list');
     commentList.empty();
-
-    if (comments.length === 0) {
-        commentList.append('<p>No comments yet.</p>');
-    } else {
-        comments.forEach(function(comment) {
-            let commentItem = "";
-            commentItem.append(`<div class="comment-content-container" id="comment_detail">
+    comments.forEach(function(comment) {
+            commentList.append(`
+            <div class="comment-content-container"> <!-- 댓글 1개-->
                 <div class="row-box comment-header">
                     <div class="commnet-name-box">
-                        <img src ="/img/miao.jpg" class="comment-profile">
+                        <img src ="/static/img/miao.jpg" class="comment-profile">
                         <span class="commnet-name">냔냔펑치</span>
                     </div>
-                    <div class="comment-day">comment.modifiedAt</div>
+                    <div class="comment-day">${comment.modifiedAt}</div>
                 </div>
-                <div class="row-box comment-article">
-                    <div> ${comment.content} </div>
+                <div class="row-box comment-article" >
+                    <div>${comment.content}</div>
                 </div>
                 <div class="row-box comment-footer">
                     <input type="button" value="댓글" class="reply-btn">
                     <input type="button" value="좋아요" class="upvote-btn">
-                    <input type="button" value="싫어요" class="downvote-btn"></div>
-            </div>`)
-            commentList.append(commentItem);
+                    <input type="button" value="싫어요" class="downvote-btn">
+                </div>
+            </div>
+        `)
         });
-    }
 }
-
-
-//
-// //댓글 조회
-// function loadComments(postId) {
-//     $.ajax({
-//         url: '/api/comment/' + postId,
-//         type: 'GET',
-//         dataType: 'json',
-//         success: function (data) {
-//             displayComments(data);
-//         },
-//         error: function(xhr, status, error) {
-//             console.error('Error fetching comments:', error);
-//         }
-//     });
-// }
-//
-//
-// function displayComments(post) {
-//     let comments = post.getComment; // post 로부터 comment 리스트를 받아와야 하는데 ~...
-//     let commentList = $('#comment_list');
-//     commentList.empty();
-//
-//     if (comments.length === 0) {
-//         commentList.append('<p>No comments yet.</p>');
-//     } else {
-//         comments.forEach(function(comment) {
-//             let commentItem = "";
-//             commentItem += `<div class="comment-content-container" id="comment_detail">
-//                 <div class="row-box comment-header">
-//                     <div class="commnet-name-box">
-//                         <img src ="/img/miao.jpg" class="comment-profile">
-//                         <span class="commnet-name">냔냔펑치</span>
-//                     </div>
-//                     <div class="comment-day">2024.03.09</div>
-//                 </div>
-//                 <div class="row-box comment-article" >
-//                     <div>` +comment.getContent+`</div> // comment 객체로부터 content도 받아와야 하고..
-//                 </div>
-//                 <div class="row-box comment-footer">
-//                     <input type="button" value="댓글" class="reply-btn">
-//                     <input type="button" value="좋아요" class="upvote-btn">
-//                     <input type="button" value="싫어요" class="downvote-btn">
-//                 </div>
-//             </div>`
-//             commentList.append(commentItem);
-//         });
-//     }
-// }
-//commit
