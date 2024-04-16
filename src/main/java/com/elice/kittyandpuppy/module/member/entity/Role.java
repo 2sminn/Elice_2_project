ce4cfc34e7f920dff0f1cc25928a3ea21fe6a45e
jaewon926 <jjojjo0926@gmail.com>
2024-04-16 15:40:46 +0900

    feat(Role) role에 string field 추가

--- src/main/java/com/elice/kittyandpuppy/module/member/entity/Role.java
+++ src/main/java/com/elice/kittyandpuppy/module/member/entity/Role.java
@@ -1,6 +1,13 @@
 package com.elice.kittyandpuppy.module.member.entity;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum Role {
-    ADMIN,
-    USER
+    ADMIN("ROLE_ADMIN"),
+    USER("ROLE_USER");
+
+    private final String role;
 }
